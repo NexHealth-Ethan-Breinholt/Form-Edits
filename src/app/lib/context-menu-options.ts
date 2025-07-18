e0ea2d56902234e6f3546ccab9c7379377c258a6
Ethan <ethan.breinholt@nexhealth.com>
2025-07-18 09:15:29 -0600

    Added context-menu-options file

---
+++ src/app/lib/context-menu-options.ts
@@ -0,0 +1,13 @@
+const CONTEXT_MENU_OPTIONS = {
+    "columns": {
+        "Split Into Columns": {
+            "sub-options": {
+                "1": 1,
+                "2": 2,
+                "3": 3,
+                "4": 4,
+                "6": 6
+            }
+        }
+    }
+}
