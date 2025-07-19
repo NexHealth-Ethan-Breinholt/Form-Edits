04122282bd03559f74e14a74384f531aa838ebdd
Ethan <ethan.breinholt@nexhealth.com>
2025-07-18 21:22:24 -0600

    Hid settings icon from Panel component

--- src/app/components/form-components/panel.tsx
+++ src/app/components/form-components/panel.tsx
@@ -8,6 +8,6 @@ interface PanelProps {
 
 export default function Panel({ path, label, children }: PanelProps) {
     return (
-        <LabelContainer componentType="panel" label={label} showLabel={true} sublabel="Panel" path={path} required={false} className={"bg-white border-zinc-300 border shadow text-zinc-700"} columnSizes={[12]} columnContent={[children]} />
+        <LabelContainer componentType="panel" label={label} showLabel={true} sublabel="Panel" path={path} showSettingsButton={false} required={false} className={"bg-white border-zinc-300 border shadow text-zinc-700"} columnSizes={[12]} columnContent={[children]} />
     );
 }
