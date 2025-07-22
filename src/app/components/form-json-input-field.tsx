fb278ca24131b078d5d12756bfecd4a5b8bbc177
Ethan <ethan.breinholt@nexhealth.com>
2025-07-22 16:20:40 -0600

    The JSON input field now updates the context's formData value

--- src/app/components/form-json-input-field.tsx
+++ src/app/components/form-json-input-field.tsx
@@ -6,6 +6,6 @@ export default function FormJsonInputField() {
     const { formData, setFormData } = useFormContext();
 
     return (
-        <textarea value={JSON.stringify(formData, null, 2)} placeholder="Enter form page JSON here..." className="w-full shadow-inner rounded-md mt-8 p-2 px-3 h-32 outline-none border border-zinc-400 resize-none text-black placeholder:italic placeholder:text-zinc-400 focus-within:border-zinc-800 focus-within:ring-4 ring-sync-200" />
+        <textarea value={JSON.stringify(formData, null, 2)} onChange={e => setFormData(JSON.parse(e.target.value))} placeholder="Enter form page JSON here..." className="w-full shadow-inner rounded-md mt-8 p-2 px-3 h-32 outline-none border border-zinc-400 resize-none text-black placeholder:italic placeholder:text-zinc-400 focus-within:border-zinc-800 focus-within:ring-4 ring-sync-200" />
     );
 }
