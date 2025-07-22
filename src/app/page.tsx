0f81726c68344181cd1fab62666b711158d8dc83
Ethan <ethan.breinholt@nexhealth.com>
2025-07-21 21:52:33 -0600

    Updated focus ring color of JSON input textarea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           </div>
           <div className="bg-white shadow p-8 text-black">
             <h1 className="font-bold text-lg">NexHealth Form Edits</h1>
-            <textarea placeholder="Enter form page JSON here..." className="w-full shadow-inner rounded-md mt-8 p-2 px-3 h-32 outline-none border border-zinc-400 resize-none text-black placeholder:italic placeholder:text-zinc-400 focus-within:border-zinc-800 focus-within:ring-4 ring-nex-green" />
+            <textarea placeholder="Enter form page JSON here..." className="w-full shadow-inner rounded-md mt-8 p-2 px-3 h-32 outline-none border border-zinc-400 resize-none text-black placeholder:italic placeholder:text-zinc-400 focus-within:border-zinc-800 focus-within:ring-4 ring-sync-200" />
             <button className="rounded-md px-4 py-1 bg-sync-200 float-right">Copy</button>
           </div>
         </main>
