c76d104d7c5a07714e0dea4349c89bcc612a5b79
Ethan <ethan.breinholt@nexhealth.com>
2025-07-23 11:17:05 -0600

    Added a legend to explain colors of components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,30 @@ export default function Home() {
               <FormDisplayer />
             </div>
           </div>
-          <div className="bg-white border-l border-zinc-300 p-8 text-black">
+          <div className="relative bg-white border-l border-zinc-300 p-8 text-black">
             <h1 className="font-bold text-lg">NexHealth Form Edits</h1>
             <FormJsonInputField />
             <div className="flex gap-2 justify-end">
               <CopyButton />
+            </div>
+            <div className="absolute bottom-4 right-4 p-4 border border-zinc-300 rounded-md">
+              <p className="font-bold text-center mb-2">Legend</p>
+              <div className="flex items-center gap-2">
+                <div className="bg-white border border-zinc-300 w-4 h-4 rounded-sm" />
+                Static
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="bg-sync-100 border border-sync-500 w-4 h-4 rounded-sm" />
+                Interactable
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="bg-zinc-100 border border-zinc-300 w-4 h-4 rounded-sm" />
+                Unimplemented
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="bg-zinc-200 w-4 h-4 rounded-sm" />
+                Background
+              </div>
             </div>
           </div>
         </main>
