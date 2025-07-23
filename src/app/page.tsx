1d70fa440bc85ea317816c42b2578f2a9c7e641e
Ethan <ethan.breinholt@nexhealth.com>
2025-07-23 11:29:30 -0600

    Updated the legend to display the hidden and disabled icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import CopyButton from "./components/copy-button";
 import FormContext from "./components/form-context";
 import FormDisplayer from "./components/form-displayer";
 import FormJsonInputField from "./components/form-json-input-field";
-import SmallButton from "./components/small-button";
+
+import { FaBan, FaEyeSlash } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -24,21 +25,35 @@ export default function Home() {
             </div>
             <div className="absolute bottom-4 right-4 p-4 border border-zinc-300 rounded-md">
               <p className="font-bold text-center mb-2">Legend</p>
-              <div className="flex items-center gap-2">
-                <div className="bg-white border border-zinc-300 w-4 h-4 rounded-sm" />
-                Static
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="bg-sync-100 border border-sync-500 w-4 h-4 rounded-sm" />
-                Interactable
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="bg-zinc-100 border border-zinc-300 w-4 h-4 rounded-sm" />
-                Unimplemented
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="bg-zinc-200 w-4 h-4 rounded-sm" />
-                Background
+              <div className="flex gap-8">
+                <div>
+                  <div className="flex items-center gap-2">
+                    <div className="bg-white border border-zinc-300 w-4 h-4 rounded-sm" />
+                    Static
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <div className="bg-sync-100 border border-sync-500 w-4 h-4 rounded-sm" />
+                    Interactable
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <div className="bg-zinc-100 border border-zinc-300 w-4 h-4 rounded-sm" />
+                    Unimplemented
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <div className="bg-zinc-200 w-4 h-4 rounded-sm" />
+                    Background
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center gap-2">
+                    <FaEyeSlash className="text-zinc-600" />
+                    Hidden
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <FaBan className="text-zinc-600" />
+                    Disabled
+                  </div>
+                </div>
               </div>
             </div>
           </div>
