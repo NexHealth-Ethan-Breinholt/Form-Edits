df75c60a2b51c569abec365fa855b23f10b86940
Ethan <ethan.breinholt@nexhealth.com>
2025-07-23 13:27:50 -0600

    Added a lable to the form JSON input field

--- src/app/components/form-json-input-field.tsx
+++ src/app/components/form-json-input-field.tsx
@@ -29,6 +29,9 @@ export default function FormJsonInputField() {
     }
 
     return (
-        <textarea value={getValue()} onChange={e => parseAndSendInput(e.target.value)} placeholder="Enter form page JSON here..." className="w-full shadow-inner rounded-md mt-8 p-2 px-3 h-32 outline-none border border-zinc-400 resize-none text-black placeholder:italic placeholder:text-zinc-400 focus-within:border-zinc-800 focus-within:ring-4 ring-sync-500" />
+        <div className="mt-8">
+            <label htmlFor="formDataInput" className="text-zinc-500 text-xs">Form Page JSON Input</label>
+            <textarea id="formDataInput" value={getValue()} onChange={e => parseAndSendInput(e.target.value)} placeholder="Enter form page JSON here..." className="w-full shadow-inner rounded-md p-2 px-3 h-32 outline-none border border-zinc-400 resize-none text-black placeholder:italic placeholder:text-zinc-400 focus-within:border-zinc-800 focus-within:ring-4 ring-sync-500" />
+        </div>
     );
 }
